9e53e96681d787785d02143a96af878463c13070
Luke Petschauer <lpetschauer@gmail.com>
2014-02-20 10:33:49 -0800

    working simple tests

--- test.py
+++ test.py
@@ -1,29 +1,27 @@
 import unittest
-
-def x(request):
-    # here I need to get a new string that goes from start to first '\r\n'
-    first_rn = request.find('\r\n')
-    first_line = request[:first_rn]
-    if first_line.split()[0] == 'GET':
-        uri = first_line.split()[1]
-        return uri
-    else:
-        raise ParseException("405: Method not allowed. Only GET is allowed.")
-
-class ParseException(Exception):
-    """An empty class to pass useful exceptions."""
-    pass
+from request_handler import *
 
 # tests to see if non-get requests raise an exception
 class ExceptionTest(unittest.TestCase):
 
     def testFailUnlessRaises(self):
-        self.failUnlessRaises(ParseException, x, 'POST /index.html HTTP/1.1 \
-                        Host: www.example.com \
+        for s in ('POST /index.html HTTP/1.1 \r\n \
+                        Host: www.example.com \r\n \
+                        <CRLF>',
+                    'PUT /index.html HTTP/1.1 \r\n \
+                        Host: www.example.com \r\n \
+                        <CRLF>',
+                    'DELETE /index.html HTTP/1.1 \r\n \
+                        Host: www.example.com \r\n \
+                       <CRLF>'):
+            self.failUnlessRaises(ParseException, parse_request, s)
+
+class EqualityTest(unittest.TestCase):
+    def testEqual(self):
+        tester = parse_request('GET /index.html HTTP/1.1 \r\n \
+                        Host: www.example.com \r\n \
                         <CRLF>')
-
-
-
+        self.assertEqual(tester, '/index.html')
 
 if __name__ == '__main__':
     unittest.main()
